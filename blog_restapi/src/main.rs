625f632079e296750747d92777ce67bfa4f794fc
vpoulo <vaughnpoulo@gmail.com>
2023-11-14 17:17:36 -0800

    add code

---
+++ blog_restapi/src/main.rs
@@ -0,0 +1,23 @@
+#[macro_use]
+extern crate rocket;
+extern crate blog_restapi;
+use rocket_contrib::templates::Template;
+
+// fn main() {
+//     rocket();
+// }
+
+#[get("/")]
+fn index() -> Template {
+    let posts: Vec<blog_restapi::Posts> = blog_restapi::read();
+    //let mut context = tera::Context::new();
+    //context.insert("posts", &posts);
+    Template::render("index", &posts)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", routes![index])
+        .attach(Template::fairing())
+}
